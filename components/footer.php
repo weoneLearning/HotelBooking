95d79651efc46fe21761ff9dd822eace7c92b2e0
weoneLearning <chariyah24112021@gmail.com>
2022-11-18 01:05:43 +0600

    hotel booking

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +269-456-7890</a>
+         <a href="tel:1112223333"><i class="fas fa-phone"></i> +269-359-2659</a>
+         <a href="mailto:shakhanas@gmail.com"><i class="fas fa-envelope"></i> weonelearning@gmail.com</a>
+         <a href="mailto:anasbhai@gmail.com"><i class="fas fa-envelope"></i> charifabdallah61@gmail.com</a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i>Moroni - Ngazidja Comoros</a>
+      </div>
+
+      <div class="box">
+         <a href="#home">home</a>
+         <a href="#about">about</a>
+         <a href="bookings.php">my bookings</a>
+         <a href="#reservation">reservation</a>
+         <a href="#gallery">gallery</a>
+         <a href="#contact">contact</a>
+         <a href="#reviews">reviews</a>
+      </div>
+
+      <div class="box">
+         <a href="https://www.facebook.com/" target="_blink">facebook <i class="fab fa-facebook-f"></i></a>
+         <a href="https://www.twitter.com/" target="_blink">twitter <i class="fab fa-twitter"></i></a>
+         <a href="https://www.instagram.com/" target="_blink">instagram <i class="fab fa-instagram"></i></a>
+         <a href="https://www.linkedin.com/" target="_blink">linkedin <i class="fab fa-linkedin"></i></a>
+         <a href="https://www.youtube.com/" target="_blink">youtube <i class="fab fa-youtube"></i></a>
+      </div>
+
+   </div>
+
+   <div class="credit">&copy; copyright @ 2021 by weone Learning | all rights reseved!</div>
+
+</section>
+
+<!-- footer section ends -->
---
+++ hotel_db.sql
@@ -0,0 +1,93 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 17, 2022 at 07:59 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hotel_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `name`, `password`) VALUES
+('EQYJaB96HcaTtxag6J7d', 'charif', 'bb031bb77d756b7813ae3aefd5167ad9960e0bbf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `user_id` varchar(20) NOT NULL,
+  `booking_id` varchar(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `number` varchar(10) NOT NULL,
+  `rooms` int(1) NOT NULL,
+  `check_in` varchar(10) NOT NULL,
+  `check_out` varchar(10) NOT NULL,
+  `adults` int(1) NOT NULL,
+  `childs` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bookings`
+--
+
+INSERT INTO `bookings` (`user_id`, `booking_id`, `name`, `email`, `number`, `rooms`, `check_in`, `check_out`, `adults`, `childs`) VALUES
+('KACtwID7zuIIXRrcGhh1', 'Eh1q8hFtEP5eAa9rDjAo', 'Charif Abdallah Yahaya Charif', 'charifabdallah61@gmail.com', '0164342398', 2, '2022-11-18', '2022-11-30', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `number` varchar(10) NOT NULL,
+  `message` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `name`, `email`, `number`, `message`) VALUES
+('WscdUnIbZp5jTwAEYof7', 'Charif Abdallah Yahaya Charif', 'charifabdallah61@gmail.com', '1643423987', 'I need a room faster');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
